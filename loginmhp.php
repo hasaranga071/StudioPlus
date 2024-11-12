24fc548298a90a11c1a927a5ff41b1d69c49871d
hasaranga071 <hasaranga@ecyber.com>
2024-11-12 22:06:30 +0530

    test change

--- loginmhp.php
+++ loginmhp.php
@@ -17,7 +17,7 @@
             margin: 0;
         }
         
-        /* Style the login form */
+
         .login-container {
             background-color: rgba(255, 255, 255, 0.8);
             padding: 20px;

e98246319f6f8f1084c7bcb79dbada69c4b881ca
hasaranga071 <hasaranga@ecyber.com>
2025-02-01 00:19:53 +0530

    login  + nav

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+#use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ToDoController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\StudioUserController;
 // Route::get('/', function () {
 //     return view('app.blade');
 // });
-Route::get('/',[HomeController::class,"index"])->name('home');
-Route::get('/neworder',[StudioUserController::class,"neworder"])->name('neworder');
-Route::post('/studio-user', [StudioUserController::class, 'store'])->name('studio-user.store');
+#Route::get('/',[HomeController::class,"index"])->name('home');
+ Route::get('/neworder',[StudioUserController::class,"neworder"])->name('neworder');
+ Route::post('/studio-user', [StudioUserController::class, 'store'])->name('studio-user.store');
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

79192d27f67e4485dc001f2b2cb906c8a6c0388b
roshanm <roshan.m@ecyber.com>
2025-03-09 23:10:43 +0530

    view order

--- resources/views/components/orderviewmodal_EC.blade.php
+++ resources/views/components/orderviewmodal_EC.blade.php
@@ -1,5 +1,5 @@
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<div class="modal fade" id="orderModal" tabindex="-1" aria-labelledby="modalTitle" aria-hidden="true">
+<div class="modal fade" id="orderModal_SS" tabindex="-1" aria-labelledby="modalTitle" aria-hidden="true">
     <div class="modal-dialog  modal-xl">
     <input type="hidden" id="otk" name="otk" value="edititem">
         <div class="modal-content">
--- resources/views/pages/todo/orders.blade.php
+++ resources/views/pages/todo/orders.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.app')
-@include('components.orderviewmodal')
+@include('components.orderviewmodal_SS')
+@include('components.orderviewmodal_EC')
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 @section('content')
@@ -86,49 +87,45 @@
 window.onload = function() {
     loaddata()
 };
+// Set default values
+document.getElementById("search-stdate").value = getFormattedDate(-30); 
+document.getElementById("search-enddate").value = getFormattedDate(+1); // Today
 
 
-//$(document).ready(function() {
-    // Existing order Search
 function loaddata()
 {
-    //$('#searchBtn').click(function () {
-            let otype = $('#search-otype').val(); 
-            let query = $('#search-term').val(); 
-            let startDate = $('#search-stdate').val(); 
-            let endDate = $('#search-enddate').val();
-            event.preventDefault(); // Prevent default form submission
-            $('#orderResults').html("");
-            $.ajax({
-                url: "/orders/search",
-                type: "POST",
-                data: {
-                    query: query,
-                    otype: otype,
-                    start_date: startDate,
-                    end_date: endDate,
-                    _token: "{{ csrf_token() }}" // CSRF Token for security
-                },
-                success: function (response) {
-                    //if (response.status) {
-                        console.log('search result',response)
-                        displayOrderSearchResults(response);
+    let otype = $('#search-otype').val(); 
+    let query = $('#search-term').val(); 
+    let startDate = $('#search-stdate').val(); 
+    let endDate = $('#search-enddate').val();
+    event.preventDefault(); // Prevent default form submission
+    $('#orderResults').html("");
+    $.ajax({
+        url: "/orders/search",
+        type: "POST",
+        data: {
+            query: query,
+            otype: otype,
+            start_date: startDate,
+            end_date: endDate,
+            _token: "{{ csrf_token() }}" // CSRF Token for security
+        },
+        success: function (response) {
+            //if (response.status) {
+                console.log('search result',response)
+                displayOrderSearchResults(response);
 
-                }
-            });
-
-       // });
-        setTimeout(function () {
-            loaddata();
-            }, 500); // 500ms delay ensures proper execution           // Trigger button click on page load
-                      
         }
- //   });
-    
+    });
 
-
-
-    function displayOrderSearchResults(orders) {
+    setTimeout(function () {
+    loaddata();
+    }, 500); // 500ms delay ensures proper execution           // Trigger button click on page load
+                        
+}
+ 
+function displayOrderSearchResults(orders) 
+{
        
       if (!orders.length) {
             $('#search-results').html(
@@ -170,7 +167,7 @@ function loaddata()
                     <td>${order.paidcost}</td>
                     <td>${order.salestatus}</td>
                     <td>
-                        <button onClick="vieworder(${order.orderkey},'${order.orderid}','${order.createdtime}','${order.username}','${order.totalcost}','${order.discount}','${order.paidcost}','${order.urgent_flag === 1 ? 'Yes' : 'No'}','${order.salestatus}',${order.ordertypekey})" id="vieword" _data-orderkey="${order.orderkey}" type="button" class="btn btn-primary view-order"  >
+                        <button onClick="vieworder(${order.orderkey},'${order.orderid}','${order.createdtime}','${order.username}','${order.totalcost}','${order.discount}','${order.paidcost}','${order.urgent_flag === 1 ? 'Yes' : 'No'}','${order.salestatus}',${order.ordertypekey},'${order.ordertype}')" id="vieword" _data-orderkey="${order.orderkey}" type="button" class="btn btn-primary view-order"  >
                         View
                         </button>
                     </td>
@@ -188,167 +185,147 @@ function loaddata()
     return date.toISOString().split('T')[0]; // Convert to YYYY-MM-DD
   }
 
-  // Set default values
-  document.getElementById("search-stdate").value = getFormattedDate(-30); 
-  document.getElementById("search-enddate").value = getFormattedDate(); // Today
+function loaditemdata_SS(okey){
+    let orderkey = okey;
+    $.ajax({
+        url: "/orders/itemsearch",
+        type: "POST",
+        data: {
+            orderkey: orderkey,
+            _token: "{{ csrf_token() }}" // CSRF Token for security
+        },
+        success: function (response) {
+                ///console.log('Itemsearch result',response)
+                displayOrderitemSearchResults(response);
 
-
-// //modal load event listner 
-//     orderModal.addEventListener('show.bs.modal', function (event) {
-        
-
-  
-//     var button = event.relatedTarget; // Button that triggered the modal
-
-//     var orderId = button.getAttribute('data-order-id'); // Get Order ID
-//     var odate = button.getAttribute('data-order-date'); 
-//     var orderKey = button.getAttribute('data-order-key'); 
-//     var otype = button.getAttribute('data-order-otype'); 
-//     var status = button.getAttribute('data-order-status'); 
-//     var urgent = button.getAttribute('data-order-urgent_flag'); 
-//     var total = button.getAttribute('data-order-total'); 
-//     var paid = button.getAttribute('data-order-paid'); 
-//     var customer = button.getAttribute('data-order-customer'); 
-
-
-//     document.getElementById('onum').textContent = orderId;
-//     document.getElementById('odate').textContent = odate;
-//     document.getElementById('customer').textContent = customer;
-//     document.getElementById('total').textContent = total;
-//     document.getElementById('paid').textContent = paid;
-//     document.getElementById('urgent').textContent = urgent;
-//     document.getElementById('status').textContent = status;
- 
-
-
-//                         loaditemdata()
-                    function loaditemdata(okey){
-                        let orderkey = okey;
-                        //$('#orderitemResults').html("");
-                        //alert('load item data='+orderkey)
-                        $.ajax({
-                            url: "/orders/itemsearch",
-                            type: "POST",
-                            data: {
-                                orderkey: orderkey,
-                                _token: "{{ csrf_token() }}" // CSRF Token for security
-                            },
-                            success: function (response) {
-                                    console.log('Itemsearch result',response)
-                                    displayOrderitemSearchResults(response);
-
-                            }
-                        });
+        }
+    });
                     
-                    }
+}
+function loaditemdata_EC(okey){
+    let orderkey = okey;
+    $.ajax({
+        url: "/orders/itemsearch_EC",
+        type: "POST",
+        data: {
+            orderkey: orderkey,
+            _token: "{{ csrf_token() }}" // CSRF Token for security
+        },
+        success: function (response) {
+                console.log('Itemsearch result EC',response)
+                displayOrderitemSearchResults_EC(response);
 
-                    function displayOrderitemSearchResults(orderitems) {
+        }
+    });
+                    
+}
+function displayOrderitemSearchResults(orderitems) {
        
-                        if (!orderitems.length) {
-                                $('#orderitemResults').html(
-                                    '<div class="alert alert-info">No Order Items found.</div>'
-                                );
-                                return;
-                            }
- 
-         let html = `
-             <table id="itemtable" class="table table-bordered">
-                 <thead>
-                     <tr>
-                         <th>Item Type</th>
-                         <th>Hard Copied</th>
-                         <th>Soft Copies</th>
-                         <th>Edit Type</th>
-                         <th>Cost (LKR)</th>
-                         <th>Status</th>
-                     </tr>
-                 </thead>
-                 <tbody>
-         `;
- 
-         orderitems.forEach(function(orderitem) {
-            
-             html += `
-                 <tr data-id="${orderitem.ssorderitemmapkey}">
-                     <td id="name_${orderitem.ssorderitemmapkey}">${orderitem.order_type_item.itemname}</td>
-                     <td id="hcopy_${orderitem.ssorderitemmapkey}">${orderitem.hardcopyquantity}</td>
-                     <td id="scopy_${orderitem.ssorderitemmapkey}">${orderitem.softcopyquantity}</td>
-                     <td id="edittype_${orderitem.ssorderitemmapkey}">${orderitem.edit_type.edittype}</td>
-                     <td>${orderitem.totalcost}</td>
-                     <td>Inprogress</td>
-                 
-
-                     <td>
-                   
-                        <button id="editBtn_${orderitem.ssorderitemmapkey}" type="button" class="btn btn-primary" onClick="edititem(${orderitem.ssorderitemmapkey},${orderitem.hardcopyquantity},${orderitem.softcopyquantity},'${orderitem.edit_type.edittype}')">
-                         Edit 
-                        </button>
-                        <button style="display:none" id="saveBtn_${orderitem.ssorderitemmapkey}" type="button" class="btn btn-primary" onClick="edititem(${orderitem.ssorderitemmapkey},${orderitem.hardcopyquantity},${orderitem.softcopyquantity},'${orderitem.edit_type.edittype}')">
-                         Save 
-                        </button>
-                     </td>
- 
-                 </tr>
-             `;
-         });
- 
-         html += '</tbody></table>';
-         $('#orderitemResults').html(html);
-     }
-             
-
-     function addnew(){
-            // Get the table body
-            let table = document.getElementById("itemtable").getElementsByTagName('tbody')[0];
-
-            // Create a new row
-            let newRow = table.insertRow();
-            newRow.style.backgroundColor = "lightblue";
-
-            // Insert cells into the row
-            let itemcell = newRow.insertCell(0);
-            let hcopycell = newRow.insertCell(1);
-            let scopycell = newRow.insertCell(2);
-            let edittypecell = newRow.insertCell(3);
-            let costcell = newRow.insertCell(4);
-            let statuscell = newRow.insertCell(5);
-            let actioncell = newRow.insertCell(6);
-
-
-            // Add content to the new cells
-            var otk=document.getElementById("otk").value;
-            itemcell.innerHTML = '<div style="width:150px;border-color: blue;border-width: 2px;" class="col-md-4" id="Sittings"> <select id="sittingitem" name="item" class="form-control" _style="width: 57%;"> <option value="">Select Item Type</option></select> </div>';
-            setTimeout(loadOrderTypeItems(otk), 3000)
-            hcopycell.innerHTML ='<div class="col-md-4" id="hcopymain"> <input id="hcopy" name="hcopy" type="text" class="form-control input-md" required=""> </div>'
-            scopycell.innerHTML ='<div class="col-md-4" id="scopymain"> <input id="scopy" name="scopy" type="text" class="form-control input-md" required=""> </div>'
-            edittypecell.innerHTML='<div class="col-md-4" id="edittypemain"><select  style="width:150px;" id="edittype" name="edittype" class="form-control"> @foreach ($editTypes as $editType) <option value="">Select Edit Type</option><option value="{{ $editType->edittypekey }}">{{ $editType->edittype }}</option> @endforeach </select> </div>'
-            costcell.innerHTML=''
-            statuscell.innerHTML=''
-            actioncell.innerHTML='<button id="addBtn" type="button" class="btn btn-primary" onClick="">Add</button>'
-        
-        
-        }
-
-        function loadOrderTypeItems(otk) {
-            var ordertypekey = otk;
-            if (ordertypekey) {
-                $.ajax({
-                    url: '/ordertypeitem/' + ordertypekey,
-                    type: 'GET',
-                    success: function (data) {
-                        console.log('aaaaaaa='+data)
-                        $('#sittingitem').empty().append('<option value="">Select an Item</option>');
-                        $.each(data, function (key, item) {
-                            $('#sittingitem').append('<option value="' + item.ordertypeitemkey + '">' + item.itemname + '</option>');
-                        });
-                    },
-                    error: function () {
-                        alert('Failed to fetch items. Please try again.');
+                if (!orderitems.length) {
+                        $('#orderitemResults').html(
+                            '<div class="alert alert-info">No Order Items found.</div>'
+                        );
+                        return;
                     }
+
+    let html = `
+        <table id="itemtable" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Item Type</th>
+                    <th>Hard Copied</th>
+                    <th>Soft Copies</th>
+                    <th>Edit Type</th>
+                    <th>Cost (LKR)</th>
+                    <th>Status</th>
+                </tr>
+            </thead>
+            <tbody>
+    `;
+
+    orderitems.forEach(function(orderitem) {
+    
+        html += `
+            <tr data-id="${orderitem.ssorderitemmapkey}">
+                <td id="name_${orderitem.ssorderitemmapkey}">${orderitem.order_type_item.itemname}</td>
+                <td id="hcopy_${orderitem.ssorderitemmapkey}">${orderitem.hardcopyquantity}</td>
+                <td id="scopy_${orderitem.ssorderitemmapkey}">${orderitem.softcopyquantity}</td>
+                <td id="edittype_${orderitem.ssorderitemmapkey}">${orderitem.edit_type.edittype}</td>
+                <td>${orderitem.totalcost}</td>
+                <td>Inprogress</td>
+            
+
+                <td>
+            
+                <button id="editBtn_${orderitem.ssorderitemmapkey}" type="button" class="btn btn-primary" onClick="edititem(${orderitem.ssorderitemmapkey},${orderitem.hardcopyquantity},${orderitem.softcopyquantity},'${orderitem.edit_type.edittype}')">
+                    Edit 
+                </button>
+                <button style="display:none" id="saveBtn_${orderitem.ssorderitemmapkey}" type="button" class="btn btn-primary" onClick="edititem(${orderitem.ssorderitemmapkey},${orderitem.hardcopyquantity},${orderitem.softcopyquantity},'${orderitem.edit_type.edittype}')">
+                    Save 
+                </button>
+                </td>
+
+            </tr>
+        `;
+    });
+
+    html += '</tbody></table>';
+    $('#orderitemResults').html(html);
+}
+             
+function addnew(){
+    // Get the table body
+    let table = document.getElementById("itemtable").getElementsByTagName('tbody')[0];
+
+    // Create a new row
+    let newRow = table.insertRow();
+    newRow.style.backgroundColor = "lightblue";
+
+    // Insert cells into the row
+    let itemcell = newRow.insertCell(0);
+    let hcopycell = newRow.insertCell(1);
+    let scopycell = newRow.insertCell(2);
+    let edittypecell = newRow.insertCell(3);
+    let costcell = newRow.insertCell(4);
+    let statuscell = newRow.insertCell(5);
+    let actioncell = newRow.insertCell(6);
+
+
+    // Add content to the new cells
+    var otk=document.getElementById("otk").value;
+    itemcell.innerHTML = '<div style="width:150px;border-color: blue;border-width: 2px;" class="col-md-4" id="Sittings"> <select id="sittingitem" name="item" class="form-control" _style="width: 57%;"> <option value="">Select Item Type</option></select> </div>';
+    setTimeout(loadOrderTypeItems(otk), 3000)
+    hcopycell.innerHTML ='<div class="col-md-4" id="hcopymain"> <input id="hcopy" name="hcopy" type="text" class="form-control input-md" required=""> </div>'
+    scopycell.innerHTML ='<div class="col-md-4" id="scopymain"> <input id="scopy" name="scopy" type="text" class="form-control input-md" required=""> </div>'
+    edittypecell.innerHTML='<div class="col-md-4" id="edittypemain"><select  style="width:150px;" id="edittype" name="edittype" class="form-control"> @foreach ($editTypes as $editType) <option value="">Select Edit Type</option><option value="{{ $editType->edittypekey }}">{{ $editType->edittype }}</option> @endforeach </select> </div>'
+    costcell.innerHTML=''
+    statuscell.innerHTML=''
+    actioncell.innerHTML='<button id="addBtn" type="button" class="btn btn-primary" onClick="">Add</button>'
+
+        
+}
+
+function loadOrderTypeItems(otk) {
+    var ordertypekey = otk;
+    if (ordertypekey) {
+        $.ajax({
+            url: '/ordertypeitem/' + ordertypekey,
+            type: 'GET',
+            success: function (data) {
+                console.log('aaaaaaa='+data)
+                $('#sittingitem').empty().append('<option value="">Select an Item</option>');
+                $.each(data, function (key, item) {
+                    $('#sittingitem').append('<option value="' + item.ordertypeitemkey + '">' + item.itemname + '</option>');
                 });
+            },
+            error: function () {
+                alert('Failed to fetch items. Please try again.');
             }
-        }
-function vieworder(key,no,odate,customer,total,discount,paid,urgent,status,otk) {
+        });
+    }
+}
+
+function vieworder(key,no,odate,customer,total,discount,paid,urgent,status,otk,ot) {
         event.preventDefault(); // Prevent default form submission
         let orderkey = $(this).data("orderkey"); // Get Order ID from button
         //alert("orderkey  no ="+key+":"+no)
@@ -360,7 +337,11 @@ function vieworder(key,no,odate,customer,total,discount,paid,urgent,status,otk) 
         $("#order-items").html("<li>Loading items...</li>");
 
         // Show the modal first
-        $("#orderModal").modal("show");
+
+
+        if (ot=='Studio Sittings') 
+        {        
+        $("#orderModal_SS").modal("show");
         $("#onum").text(no);
         $("#odate").text(odate);
         $("#customer").text(customer);
@@ -369,12 +350,28 @@ function vieworder(key,no,odate,customer,total,discount,paid,urgent,status,otk) 
         $("#paid").text(paid);
         $("#urgent").text(urgent);
         $("#status").text(status);
+        loaditemdata_SS(key)
+    }
+        
+    if (ot=='Extra Copy') 
+    {
+        $("#orderModal_EC").modal("show");
+        $("#onum").text(no);
+        $("#odate").text(odate);
+        $("#customer").text(customer);
+        $("#total").text(total);
+        $("#discount").text(discount);
+        $("#paid").text(paid);
+        $("#urgent").text(urgent);
+        $("#status").text(status);
+        loaditemdata_EC(key)
 
-        loaditemdata(key)
+    }
+        
 
         $(this).off("shown.bs.modal");
 
-        }
+}
  
 
 </script>

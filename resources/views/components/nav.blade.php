a7f0af56a9c604967aa27df689941b1bc0e2dc25
hasaranga071 <hasaranga@ecyber.com>
2025-04-05 23:43:58 +0530

    dashboard page change

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -14,7 +14,7 @@
 			<button class="btn-primary" style="border-radius:5px" type="button" onclick="window.open('/neworder')">NEW ORDER</button>
 		</ul>
 		<ul class="custom-navbar-nav navbar-nav ">
-			<li _class="nav-item active"> <a class="navbar-link" href="/ordertypesummary">Dashboard</</li>
+			<li _class="nav-item active"> <a class="navbar-link" href="/index">Dashboard</</li>
 			<li><a class="navbar-link" href="/orders">Orders</a></li>
 			<!-- <li><a class="navbar-link" href="/ordertypesummary">My Business</a></li>
 			<li><a class="navbar-link" href="services.html">Configurations</a></li> -->
